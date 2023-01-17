e45761d3c5cfbacbb22e8fd9b15c0ef7345a0ad8
“andrelugo” <andrelugolopez@gmail.com>
2023-01-16 20:38:14 -0500

    Maquetación vistas mecanico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {ContainerLoginComponent} from './components/container-login/container-login.component';
+import {ContainerMenuMecanicoComponent} from './components/container-menu-mecanico/container-menu-mecanico.component';
+import {ContainerDetallesReparacionComponent} from './components/container-detalles-reparacion/container-detalles-reparacion.component';
+import {ContainerHistorialComponent} from './components/container-historial/container-historial.component';
+import {ContainerListaReparacionesComponent} from './components/container-lista-reparaciones/container-lista-reparaciones.component';
 
 const routes: Routes = [
-  {path:'login', component: ContainerLoginComponent},
+
+  {path:'', component: ContainerLoginComponent}, 
+  {path:'mecanico', component: ContainerMenuMecanicoComponent},
+  {path:'detallesReparacion', component: ContainerDetallesReparacionComponent},
+  {path:'historial', component: ContainerHistorialComponent},
+  {path:'listaReparacion', component: ContainerListaReparacionesComponent},
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { MenuMecanicoComponent } from './components/menu-mecanico/menu-mecanico.component';
+import { ContainerMenuMecanicoComponent } from './components/container-menu-mecanico/container-menu-mecanico.component';
 import { LoginComponent } from './components/login/login.component';
 import { ContainerLoginComponent } from './components/container-login/container-login.component';
+import { ContainerListaReparacionesComponent } from './components/container-lista-reparaciones/container-lista-reparaciones.component';
+import { ListaReparacionesComponent } from './components/lista-reparaciones/lista-reparaciones.component';
+import { ContainerDetallesReparacionComponent } from './components/container-detalles-reparacion/container-detalles-reparacion.component';
+import { DetallesReparacionComponent } from './components/detalles-reparacion/detalles-reparacion.component';
+import { ContainerHistorialComponent } from './components/container-historial/container-historial.component';
+import { HistorialComponent } from './components/historial/historial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuMecanicoComponent,
+    ContainerMenuMecanicoComponent,
     LoginComponent,
-    ContainerLoginComponent
+    ContainerLoginComponent,
+    ContainerListaReparacionesComponent,
+    ListaReparacionesComponent,
+    ContainerDetallesReparacionComponent,
+    DetallesReparacionComponent,
+    ContainerHistorialComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
